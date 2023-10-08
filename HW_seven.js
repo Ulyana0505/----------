7ca83043d6326e2d3b9a0a6bbd11145c8cf01bbf
Ulyana <lana.arfinova@gmail.com>
2023-10-08 21:46:28 +0300

    dont work ehhh

---
+++ HW_seven.js
@@ -0,0 +1,25 @@
+//document.getElementById('input')[0].addEventListener('input', function(e) {
+//   if (this.value) {
+//       document.getElementById('b').classList.add('show');
+//    } else {
+//        document.getElementById('b').classList.remove('show');
+//   }
+//});
+
+
+
+
+//$("input").on("input", function() {
+//   if ($(this).val()) {
+//       $("button").show();
+//   } else {
+//        $("button").hide();
+//    }
+//});
+//$("button").click(function() {
+//    $("input").val('');
+//    $(this).hide();
+//});
+
+//let button = document.getElementById("button");
+//button.style.display = "none";
