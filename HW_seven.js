4c4e1d4cfda5ab88f34a3dd10ab3f6b8acae3d28
Ulyana <lana.arfinova@gmail.com>
2023-10-09 22:01:09 +0300

    ALL DONE

--- HW_seven.js
+++ HW_seven.js
@@ -15,15 +15,16 @@ function change() {
     }
 }
 
-    // 2
+    // 2 + 3
 function click() {
     let p = document.createElement('p');
+    p.className = "selector";
     document.body.appendChild(p);
     p.innerHTML = input.value;
+    let pList = document.querySelectorAll("p.selector");
+    console.log(pList.length);
+    if(pList.length > 5) {
+    pList[0].remove();
+    }
 }
 button.onclick = click;
-
-    // 3
-if(p > 5) {
-    p[0].remove;
-}
